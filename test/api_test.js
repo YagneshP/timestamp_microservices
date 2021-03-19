ae1cd31d2a3c4a19435e3f46d7787b4bfb0eefe9
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-03-19 14:57:09 -0400

    test: add http test fot get timestamp route

---
+++ test/api_test.js
@@ -0,0 +1,24 @@
+const { assert } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+
+describe('/GET/:date? timestamp', function(){
+	it('it should return json with unix and utc', (done)=>{
+		let date = '1451001600000';
+		chai.request(server)
+				.get('/api/timestamp/' + date)
+				.send({unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"})
+				.end((err,res)=>{
+					assert.equal(res.status, 200, 'response status should be 200');
+					assert.equal(res.type,'application/json');
+					assert.equal(res.body.unix,1451001600000);
+					assert.equal(res.body.utc,'Fri, 25 Dec 2015 00:00:00 GMT');
+				});
+				done();
+	})
+
+})
