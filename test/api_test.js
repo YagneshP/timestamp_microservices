739cd0e07d5e212aaca9879db847db9b8d0c3498
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-03-19 19:26:08 -0400

    test: add test for empty date

--- test/api_test.js
+++ test/api_test.js
@@ -5,7 +5,6 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-
 describe('/GET/:date? timestamp', function(){
 	it('it should return json with unix and utc', (done)=>{
 		let date = '1451001600000';
@@ -20,5 +19,20 @@ describe('/GET/:date? timestamp', function(){
 				});
 				done();
 	})
-
+});
+describe('/GET/api/timestamp', function(){
+	it('it should return json with unix and utc value with current date/time', (done)=>{
+		let unixDate = Date.now();
+		let utcDate = Date()
+		chai.request(server)
+				.get('/api/timestamp/')
+				.send({unix: unixDate, utc: utcDate})
+				.end((err,res)=>{
+					assert.equal(res.status, 200, 'response status should be 200');
+					assert.equal(res.type,'application/json');
+					assert.equal(res.body.unix,unixDate);
+					assert.equal(res.body.utc,utcDate);
+				});
+				done();
+	})
 })
